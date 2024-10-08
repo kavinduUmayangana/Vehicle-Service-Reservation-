674ba91bb19789e7a0fbda6cb73e808fd10fc0b2
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2024-10-08 18:47:52 +0530

    create the responsive navbar

---
+++ FrontEnd/ui/src/navbar.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import CloseIcon from '@mui/icons-material/Close';
+
+const Header = () => {
+  const Links = [
+    { name: "Home",            link: "Home" },
+    { name: "about", link: "About" },
+    { name: "Contact",         link: "Contact"},
+    
+  ];
+
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+  
+
+  return (
+    <header className='fixed top-0 left-0 z-10 w-full shadow-md bg-blue-300' 
+>
+      <nav className='items-center justify-between py-4 md:flex md:px-10 px-7'>
+        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-950'>
+          AutoKAVEE
+        </div>
+        <div onClick={toggleMenu} className='absolute text-2xl cursor-pointer right-8 top-3 md:hidden'>
+          {open ? <CloseIcon /> : <DensityMediumIcon />}
+        </div>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-blue-300 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
+ ${open ? 'top-15 opacity-100':'top-[-490px]'} md:opacity-100 opacity-0`}>
+          {Links.map((link) => (
+            <li key={link.name} className='text-xl md:ml-8 md:my-0 my-7' >
+              <ScrollLink to={link.link}
+                          smooth={true}
+                          duration={500}
+               onClick={()=> setOpen(false)}
+              >
+                {link.name}
+              </ScrollLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
