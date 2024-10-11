1277d1a596576010979fcb65451380bb42cdf24b
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2024-10-11 11:17:00 +0530

    create the backend Server

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const express =require("express");
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome tfffffffffffffffffffffo my server!');
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000`);
+});
